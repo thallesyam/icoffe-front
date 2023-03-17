edf967e4d8b40db84a8e7e4f408b606c80dd2cb0
thallesyam <thallesyam@gmail.com>
2023-03-17 16:03:13 -0300

    feat: config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from '@/Router'
+
 export function App() {
-  return <h1 className="text-orange-900">Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
