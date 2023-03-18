6d74d929f35acf7d658e94fefec8cec8b618f697
thallesyam <thallesyam@gmail.com>
2023-03-18 12:44:54 -0300

    feat: create login with google provider

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import { MapPin, User, Bank } from 'phosphor-react'
+import { Link, useLocation } from 'react-router-dom'
 import { Minicart } from '@/components/Minicart'
+import { getGoogleUrl } from '@/service/getUrlFromGoogle'
 
 export function Header() {
   const hasUser = false
+  const location = useLocation()
+  const from = ((location.state as any)?.from.pathname as string) || '/profile'
 
   return (
     <header className="flex justify-between items-center py-8 px-2">
@@ -11,20 +15,26 @@ export function Header() {
       </div>
 
       <div className="flex items-center gap-3">
-        {hasUser ? (
+        {!hasUser ? (
           <div className="flex items-center gap-2">
-            <button className="bg-purple-50 p-2 flex items-center gap-1 rounded-md hover:opacity-90 hover:transition-all">
+            <Link
+              to={getGoogleUrl(from)}
+              className="bg-purple-50 p-2 flex items-center gap-1 rounded-md hover:opacity-90 hover:transition-all"
+            >
               <Bank size={22} weight="fill" className="text-purple-500" />
               <p className="text-purple-900 font-normal text-sm ">
                 Entre como empresa
               </p>
-            </button>
-            <button className="bg-yellow-50 p-2 flex items-center gap-1 rounded-md hover:opacity-90 hover:transition-all">
+            </Link>
+            <Link
+              to={getGoogleUrl(from)}
+              className="bg-yellow-50 p-2 flex items-center gap-1 rounded-md hover:opacity-90 hover:transition-all"
+            >
               <User size={22} weight="fill" className="text-yellow-500" />
               <p className="text-yellow-900 font-normal text-sm ">
                 Entre como usuário
               </p>
-            </button>
+            </Link>
           </div>
         ) : (
           <div className="flex items-center gap-2">
---
+++ src/service/getUrlFromGoogle.ts
@@ -0,0 +1,21 @@
+export const getGoogleUrl = (from: string) => {
+  const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`
+
+  const options = {
+    redirect_uri: 'http://localhost:5000/api/auth/google/callback',
+    client_id:
+      '521382849500-4u14m8gnh9d3kbh8nhq8ge49lsq48vkc.apps.googleusercontent.com',
+    access_type: 'offline',
+    response_type: 'code',
+    prompt: 'consent',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+    ].join(' '),
+    state: from,
+  }
+
+  const qs = new URLSearchParams(options)
+
+  return `${rootUrl}?${qs.toString()}`
+}
