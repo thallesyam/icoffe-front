c26bfd8db416f3f7718148ae9239dc14eaf20531
thallesyam <thallesyam@gmail.com>
2023-03-23 18:14:26 -0300

    change enviroment

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+const dev = false
+const env = dev ? 'http://localhost:5000' : 'https://icoffee.onrender.com'
+
 export const api = axios.create({
-  baseURL: 'https://icoffee.onrender.com',
+  baseURL: env,
 })
--- src/service/getUrlFromGoogle.ts
+++ src/service/getUrlFromGoogle.ts
@@ -1,8 +1,11 @@
+const dev = false
+const env = dev ? 'http://localhost:5000' : 'https://icoffee.onrender.com'
+
 export const getGoogleUrl = (from: string) => {
   const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`
 
   const options = {
-    redirect_uri: 'https://icoffee.onrender.com/api/auth/google/callback',
+    redirect_uri: `${env}/api/auth/google/callback`,
     client_id:
       '521382849500-4u14m8gnh9d3kbh8nhq8ge49lsq48vkc.apps.googleusercontent.com',
     access_type: 'offline',
