94e1a91b9f61f74452e90a12307d78268005427c
thallesyam <thallesyam@gmail.com>
2023-03-23 08:56:20 -0300

    adjusting redirect

--- src/service/getUrlFromGoogle.ts
+++ src/service/getUrlFromGoogle.ts
@@ -2,7 +2,7 @@ export const getGoogleUrl = (from: string) => {
   const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`
 
   const options = {
-    redirect_uri: 'http://localhost:5000/api/auth/google/callback',
+    redirect_uri: 'https://icoffee.onrender.com/api/auth/google/callback',
     client_id:
       '521382849500-4u14m8gnh9d3kbh8nhq8ge49lsq48vkc.apps.googleusercontent.com',
     access_type: 'offline',
